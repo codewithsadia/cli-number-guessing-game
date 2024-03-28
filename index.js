7dcbb45a4231e060aebc09116fd9679c6623caea
sadia <sadiaimranmalik24@gmail.com>
2024-03-28 10:16:53 +0500

    add

---
+++ index.js
@@ -0,0 +1,17 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+console.log("Wellcome to codewithsadia - Cli Number guessing game ");
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuestNumber",
+        type: "number",
+        message: "Enter your guesst number(Number Limit from 1 to 5 ):",
+    },
+]);
+if (answer.userGuestNumber === randomNumber) {
+    console.log("Congratulation ! you guess a correct number.");
+}
+else {
+    console.log("Sorry you guess a wrong number.");
+}
